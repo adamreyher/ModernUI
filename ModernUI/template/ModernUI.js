0103023676f44f1ea5ee6a1fa57fa16cb9b6c676
Adam Reyher <adamr@phpbb.com>
2010-11-04 14:40:48 -0400

    Initial template structure. Added jQuery and ModernUI JavaScript files CSS files added Cleaned up contrib/CREDITS

---
+++ ModernUI/template/ModernUI.js
@@ -0,0 +1,11 @@
+/*
+ * ModernUI
+ * Copyright 2010 Adam Reyher, 2010 Titanium Studio
+ * http://www.titaniumstudio.com
+ *
+ * This file is licensed under the MIT license
+ * http://www.opensource.org/licenses/mit-license.php
+ * See ../contrib/CREDITS for licensing details
+ *
+ * This file contains JavaScript code unique to the ModernUI style.
+ */
